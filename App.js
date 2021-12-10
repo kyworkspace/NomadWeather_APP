02c7a9bdaa7553294258a84447b3b336f4243ec3
Park Ki Young <ky91zzang@naver.com>
2021-12-10 10:58:35 +0900

    weather api

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
@@ -9,55 +10,57 @@ import {
   Dimensions,
   View,
   PermissionsAndroid,
-  Platform
+  Platform,
+  ActivityIndicator
 } from 'react-native';
 
 import Geolocation from 'react-native-geolocation-service';
 
 const {height,width:SCREEN_WIDTH} = Dimensions.get('window')
 
-
-
-
-
-
-
-
-
+const API_KEY = 'API_KEYS';
 
 const App = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-
+  const [days, setDays] = useState([])
+  const [ok, setOk] = useState(false);
+  const [timeZone, setTimeZone] = useState('Loading...')
 
   const ask = async() =>{
     if(!location){
       Geolocation.getCurrentPosition((position)=>{
-        console.log(position)
+        setLocation(position);
+        getWeather(position.coords.latitude,position.coords.longitude)
+        setOk(true);
       },
       (error)=>{
         console.log(error)
-        const {code} = error;
-        if(code === 1){
-          setErrorMsg('위치 접근 권한이 없습니다.');
-          alert('위치 접근 권한이 없습니다.')
-        }
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 })
     }
   }
 
+  const getWeather=async(lat, lon)=>{
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts&appid=${API_KEY}&units=metric`)
+    const json = await result.json();
+    setTimeZone(json.timezone);
+    setDays(json.daily)
+  }
+
   const requestPermission = async ()=>{
     if(Platform.OS === 'android'){
-      console.log('안드로이드');
       await PermissionsAndroid.requestMultiple([
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
       ])
       .then((result)=>{
-        console.log('result',result)
-        if(result['PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION'] ==='granted'){
-          console.log('카메라')
+        if(result['android.permission.ACCESS_FINE_LOCATION'] && result['android.permission.ACCESS_COARSE_LOCATION'] ==='granted'){
+          ask();
+        }else{
+          setErrorMsg('위치 접근 권한이 없습니다.');
+          alert('위치 접근 권한이 없습니다.')
+          setOk(false)
         }
       })
     }
@@ -80,7 +83,7 @@ const App = () => {
     <SafeAreaView style={styles.container}>
       <StatusBar color={'tomato'} hidden={false} />
       <View style={styles.city}>
-        <Text style={styles.cityName}>서울</Text>
+        <Text style={styles.cityName}>{timeZone.toUpperCase()}</Text>
       </View>
       <ScrollView 
         horizontal 
@@ -89,30 +92,21 @@ const App = () => {
         contentContainerStyle={styles.weather}
 
         >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ?
+          <View style={styles.day}>
+            <ActivityIndicator size={'large'} color={'white'} />
+          </View>
+          : days.map((item, idx) => {
+            const date = moment(item.dt*1000);
+            return (
+              <View style={styles.day} key={idx}>
+                <Text>{`${date.month()+1}월 ${date.date()}일`}</Text>
+                <Text style={styles.temp}>{parseFloat(item.temp.day).toFixed(1)}</Text>
+                <Text style={styles.description}>{item.weather[0].main}</Text>
+                <Text style={styles.tinyText}>{item.weather[0].description}</Text>
+              </View>
+            )
+          })}
       </ScrollView>
     </SafeAreaView>
   );
@@ -146,6 +140,9 @@ const styles = StyleSheet.create({
   description:{
     fontSize:60,
     marginTop:-30
+  },
+  tinyText:{
+    fontSize:20
   }
 
 })
